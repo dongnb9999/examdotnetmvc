4382db53e67cdeb62c12f12f3ced82dc39d323d5
Quocanh <quocanh16999@gmail.com>
2019-11-23 01:07:21 +0700

    ex

---
+++ exam/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace exam.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=Exam") { }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+
+}
